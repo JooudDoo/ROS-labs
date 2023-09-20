502fc0d486e6111844ab9c53324e7ae84a3e8417
Joo <jooud@da.da>
2023-09-20 21:46:49 +0700

    ex01

---
+++ module03/ex01/service_full_name/src/client_name.cpp
@@ -0,0 +1,48 @@
+#include "rclcpp/rclcpp.hpp"
+#include "name_services/srv/name.hpp"
+
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  if (argc != 4) {
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: full name client first name last");
+      return 1;
+  }
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("name_client");
+  rclcpp::Client<name_services::srv::Name>::SharedPtr client =
+    node->create_client<name_services::srv::Name>("full_name");
+
+  auto request = std::make_shared<name_services::srv::Name::Request>();
+  request->first_name = (argv[1]);
+  request->name = (argv[2]);
+  request->last_name = (argv[3]);
+
+  while (!client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+  }
+
+  auto result = client->async_send_request(request);
+
+  if (rclcpp::spin_until_future_complete(node, result) ==
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Full name: %s", result.get()->full_name.c_str());
+  } else {
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service service_full_name");
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
